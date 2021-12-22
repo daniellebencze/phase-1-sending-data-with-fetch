eced20df721a421d2413cdd46a5505fd7b6607bb
Danielle Bencze <daniellebencze@live.com>
2021-12-22 10:32:25 -0800

    commit tree 57f32ce0cff3c12a8783ef9931d644bf8f946163

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+const user = {
+  name: "Danielle",
+  email: "d@gmail.com",
+};
+
+function submitData(name, email) {
+  fetch("http://localhost:3000/users"),
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(user),
+    };
+}
+
+// function submitData() {
+//   //   // To pass the tests, don't forget to return your fetch!
+//   return fetch("http://localhost:3000/users")
+//     .then((resp) => resp.json())
+//     .then(function (json) {
+//       return submitData(json);
+//     });
+// }
